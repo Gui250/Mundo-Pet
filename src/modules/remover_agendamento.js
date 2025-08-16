d8c08ef29c5f2732758e699b30014387caf28f66
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-15 21:49:46 -0300

    botão de remover funcionando

--- src/modules/remover_agendamento.js
+++ src/modules/remover_agendamento.js
@@ -1,1 +1,9 @@
-const removerAgendamento = document.getElementsByTagName("a");
+document.querySelectorAll(".descricao-agendamento a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault(); // evita o comportamento padrão
+    let elemento = link.parentNode; // começa pelo pai direto do link
+    if (elemento && elemento.classList.contains("descricao-agendamento")) {
+      elemento.remove(); // remove a div do agendamento
+    }
+  });
+});
