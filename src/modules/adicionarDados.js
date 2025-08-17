db1abf6cc63efe618e31719dc498a79cd18dea7e
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-17 09:58:32 -0300

    alterações feitas

--- src/modules/adicionarDados.js
+++ src/modules/adicionarDados.js
@@ -1,8 +1,3 @@
-const descricaoAgendamentoMorning = document.getElementById("descricao-agendamento-morning");
-const descricaoAgendamentoAfternoon = document.getElementById("descricao-agendamento-afternoon");
-const descricaoAgendamentoNight = document.getElementById("descricao-agendamento-night");
-
-
 const nomeTutor = document.getElementById("nomeTutor");
 const nomePet = document.getElementById("nomePet");
 const telefone = document.getElementById("telefone");
@@ -12,25 +7,9 @@ const horario = document.getElementById("horario");
 let horarios = [];
 
 function filtro() {
-    let valorHorario = horario.value;
-    let valorTutor = nomeTutor.value;
-    let valorPet = nomePet.value;
-    let valorDescricaoServico = descricaoServico.value;
-    let valorData = data.value;
-
-
-
-
-    horarios.push(valorHorario);
-
-    let horariosFiltrados = horarios.filter((horario) => { 
-        if(horario.value >= "09:00" && horario.value <= "12:00") {
-            const dados = [valorTutor, valorPet, valorDescricaoServico, valorData, valorHorario];
-
-            for(let i = 0 ; i <= dados.length; i++) { 
-                
-            }
-
-        }
-    })
+  let valorHorario = horario.value;
+  let valorTutor = nomeTutor.value;
+  let valorPet = nomePet.value;
+  let valorDescricaoServico = descricaoServico.value;
+  let valorData = data.value;
 }
